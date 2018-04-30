81d0891eca3f558a9fe14a0e63874ba1b7f8f7ba
Usuario <Usuario@Malen>
2018-04-30 19:31:23 -0300

    punto 1 completo

--- src/java/aplicacion/controlador/beans/forms/temperaturaFormBean.java
+++ src/java/aplicacion/controlador/beans/forms/temperaturaFormBean.java
@@ -1,25 +1,65 @@
+package aplicacion.controlador.beans.forms;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aplicacion.controlador.beans.forms;
 
+import aplicacion.modelo.dominio.Temperatura;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 /**
  *
- * @author Martin
+ * @author Usuario
  */
 @ManagedBean
 @ViewScoped
 public class temperaturaFormBean {
+    private int opcion;
+    private double grado;
+    private Temperatura temperatura;
 
     /**
      * Creates a new instance of temperaturaFormBean
+     * @return 
      */
+    
+  public double calcularTemperatura(){
+      return temperatura.calcularTemperatura(grado,opcion);
+  }
+    
+    
     public temperaturaFormBean() {
+        temperatura=new Temperatura();
     }
+
+    public double getGrado() {
+        return grado;
+    }
+
+    public void setGrado(double grado) {
+        this.grado = grado;
+    }
+
+    public int getOpcion() {
+        return opcion;
+    }
+
+    public void setOpcion(int opcion) {
+        this.opcion = opcion;
+    }
+    
+
+    public Temperatura getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(Temperatura temperatura) {
+        this.temperatura = temperatura;
+    }
+    
+    
     
 }
--- src/java/aplicacion/modelo/dominio/temperatura.java
+++ src/java/aplicacion/modelo/dominio/temperatura.java
@@ -5,10 +5,31 @@
  */
 package aplicacion.modelo.dominio;
 
+import java.io.Serializable;
+
 /**
  *
- * @author Martin
+ * @author Usuario
  */
-public class temperatura {
+public class Temperatura implements Serializable {
+ 
     
+    public double calcularTemperatura(double grado, int opcion){
+        double c=0;
+       if (opcion==1){
+       c=grado;
+       }
+       else{
+           if (opcion==2){
+           c=grado+ 273.15;
+           }
+           else{
+                if(opcion==3){
+                    c=(grado* 1.8)+32;
+                }
+                              
+           }
+       }
+        return c;
+    }
 }
